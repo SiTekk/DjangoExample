fd434d93ff63f5bab651f39f6f6b2fbeac986c7e
SiTek <siekie41@googlemail.com>
2022-07-07 12:10:51 +0200

    removed hardcoded key

--- DjangoExample/settings.py
+++ DjangoExample/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&dfgizvmmi*irx94up%%qiv#eol%)9xr-bwcdm&!=n-=bxm=-!'
+with open("DjangoExample/key.txt") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
